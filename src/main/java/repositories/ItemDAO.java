f7d18f2c06cb5a860ebd5c6b45eb3a2988876369
franklinosei <franklinosei15@gmail.com>
2023-05-24 12:08:35 +0000

    Added methods to dao

--- src/main/java/repositories/ItemDAO.java
+++ src/main/java/repositories/ItemDAO.java
@@ -6,7 +6,11 @@ package repositories;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import models.ItemsModel;
 
 /**
  *
@@ -20,16 +24,14 @@ public class ItemDAO {
 
         this.conn = conn;
     }
-    
-    
-    
-    public int insertItem(Item item) throws Exception {
+
+    public int insertItem(ItemsModel item) throws Exception {
 
         try {
             // Insert query
             String query = "INSERT INTO items (genre_name) VALUES (?)";
             PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, item.getItemName());
+            stmt.setString(1, item.getName());
 
             int rowsAffected = stmt.executeUpdate();
 
@@ -40,4 +42,89 @@ public class ItemDAO {
         }
 
     }
+
+    public int updateItem(ItemsModel item) throws Exception {
+
+        try {
+            // Update data
+            String updateQuery = "UPDATE genres SET genre_name = ? WHERE genre_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(updateQuery);
+            stmt.setString(1, item.getName());
+            stmt.setInt(2, item.getItemID());
+
+            int rowsAffected = stmt.executeUpdate();
+            stmt.close();
+            return rowsAffected;
+
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+    
+        public int deleteItem(ItemsModel item) throws Exception {
+
+        try {
+            // Update data
+            String updateQuery = "UPDATE items SET deleted = ? WHERE genre_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(updateQuery);
+            stmt.setBoolean(1, item.getDeleted());
+            stmt.setInt(2, item.getItemID());
+
+            int rowsAffected = stmt.executeUpdate();
+            stmt.close();
+            return rowsAffected;
+
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+//    public ItemsModel getItemByID(int id) throws Exception {
+//
+//        try {
+//            // Update data
+//            String updateQuery = "SELECT * FROM items WHERE id = ?";
+//            PreparedStatement stmt = conn.prepareStatement(updateQuery);
+//            stmt.setInt(1, id);
+//
+//            int rowsAffected = stmt.executeUpdate();
+//            stmt.close();
+//            
+//            return rowsAffected;
+//
+//        } catch (SQLException e) {
+//            throw new Exception(e.getMessage());
+//        }
+//    }
+
+    
+    
+    public ArrayList<ItemsModel> getAllItems() throws Exception {
+
+        try {
+            ArrayList<ItemsModel> itemsList = new ArrayList<>();
+            // Make query
+            String query = "SELECT genre_id, genre_name FROM genres";
+            Statement stmt = this.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+
+                String name = rs.getString("genre_name");
+                int id = rs.getInt("genre_id");
+
+            // TODO: Update the models to access attributes via constructor
+                ItemsModel item = new ItemsModel();
+                itemsList.add(item);
+            }
+            stmt.close();
+            rs.close();
+            return itemsList;
+
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        }
+
+    }
+
 }
